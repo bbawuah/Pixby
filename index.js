f5ca93cc686a6ff3d8471c0fc15599b948b97881
bbawuah <brianbawuah@icloud.com>
2020-06-09 15:44:28 +0200

    stashing changes

--- index.js
+++ index.js
@@ -1,64 +1,32 @@
+require("dotenv").config();
 // packages
-const hbs = require('hbs');
-const express = require('express');
-const bodyParser = require('body-parser');
+const hbs = require("hbs");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express(); // opstarten van express applicatie
 const port = 4000;
-const path = require('path');
-require('dotenv').config();
+const path = require("path");
 
-const url = process.env.MONGO_URL;
-console.log(url);
-
-require('./src/db/mongoose.js');
-
-const {
-  MongoClient
-} = require('mongodb');
-
-const session = require('express-session');
-
-
-
-let db = null;
-let usersList = null;
 
 // middleware
 app
-  .set('view engine', 'hbs')
-  .set('views', 'views')
-  .use(express.static('public')) // gebruikt deze map (public) om html bestanden te serveren
-  .use(session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: true,
-    resave: false,
-    secure: true,
-  }));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-hbs.registerPartials(path.join(__dirname, '/views/partials'));
-
-
-// DATABASE CONNECTION
-// MongoClient.connect(url, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }, (err, client) => {
-//   if (err) {
-//     console.log('Unable to connect to database');
-//   }
-//   db = client.db(process.env.DB_NAME);
-//   usersList = db.collection('users');
-// });
+  .set("view engine", "hbs")
+  .set("views", "views")
+  .use(express.static("public")); // gebruikt deze map (public) om html bestanden te serveren
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
 
 // inlogpagina waar alle session gebruikers worden weergeven
-app.get('/signin', async (req, res, next) => {
+app.get("/signin", async (req, res, next) => {
   try {
     const fromDatabase = await usersList.find().toArray();
-    res.render('signin', {
-      title: 'signin',
-      users: fromDatabase
+    res.render("signin", {
+      title: "signin",
+      users: fromDatabase,
     });
   } catch (error) {
     next(error);
@@ -66,52 +34,60 @@ app.get('/signin', async (req, res, next) => {
 });
 
 // hier wordt je doorgestuurd naar de indexpagina
-app.post('/loading', async (req, res, next) => {
+app.post("/loading", async (req, res, next) => {
   try {
     req.session.name = req.body.name;
     console.log(req.session.name);
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     next(error);
   }
 });
 
 // indexpagina
-app.get('/', async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   try {
     // elke keer de server opnieuw start
     // redirect je naar inlogpagina
     if (req.session.name === undefined) {
-      res.redirect('/signin');
+      res.redirect("/signin");
     }
     // haalt session gebruiker uit de database
-    let signedUser = await usersList.find({
-      name: req.session.name
-    }).toArray();
+    let signedUser = await usersList
+      .find({
+        name: req.session.name,
+      })
+      .toArray();
     // alle gebruikers uit de database gehaald zonder signedUser mee te nemen
-    const allBabies = await usersList.find({
-      $and: [{
-        name: {
-          $ne: signedUser[0].name
-        },
-      }, {
-        name: {
-          // array waar alle gelikete users in worden opgeslagen
-          // waardoor hij niet meer zichtbaar is op de index
-          $nin: Object.values(signedUser[0].liked)
-        },
-      }, {
-        name: {
-          // array waar alle gelikete users in worden opgeslagen
-          // waardoor hij niet meer zichtbaar is op de index
-          $nin: Object.values(signedUser[0].disliked)
-        },
-      }]
-    }).toArray();
+    const allBabies = await usersList
+      .find({
+        $and: [
+          {
+            name: {
+              $ne: signedUser[0].name,
+            },
+          },
+          {
+            name: {
+              // array waar alle gelikete users in worden opgeslagen
+              // waardoor hij niet meer zichtbaar is op de index
+              $nin: Object.values(signedUser[0].liked),
+            },
+          },
+          {
+            name: {
+              // array waar alle gelikete users in worden opgeslagen
+              // waardoor hij niet meer zichtbaar is op de index
+              $nin: Object.values(signedUser[0].disliked),
+            },
+          },
+        ],
+      })
+      .toArray();
     // allBabies wordt gerendert naar de index
-    res.render('index', {
-      title: 'home',
-      users: allBabies
+    res.render("index", {
+      title: "home",
+      users: allBabies,
     });
   } catch (error) {
     next(error);
@@ -119,40 +95,49 @@ app.get('/', async (req, res, next) => {
 });
 
 // gelikete user wordt doorgestuurd naar match pagina
-app.post('/match', async (req, res, next) => {
+app.post("/match", async (req, res, next) => {
   try {
-    const signedUser = await usersList.find({
-      name: req.session.name
-    }).toArray();
+    const signedUser = await usersList
+      .find({
+        name: req.session.name,
+      })
+      .toArray();
     // Als je iemand liked of disliked wordt het hele object
     // van de gebruiker gepusht naar je liked of disliked array
     const updateLikedUsers = () => {
       if (req.body.like) {
-        usersList.updateOne({
-          name: signedUser[0].name,
-        }, {
-          $push: {
-            liked: req.body.like,
+        usersList.updateOne(
+          {
+            name: signedUser[0].name,
           },
-        });
+          {
+            $push: {
+              liked: req.body.like,
+            },
+          }
+        );
         return true;
       }
     };
     const updateDislikedUsers = () => {
       if (req.body.dislike) {
-        usersList.updateOne({
-          name: signedUser[0].name,
-        }, {
-          $push: {
-            disliked: req.body.dislike,
+        usersList.updateOne(
+          {
+            name: signedUser[0].name,
           },
-        });
+          {
+            $push: {
+              disliked: req.body.dislike,
+            },
+          }
+        );
         return false;
       }
     };
     // het hele object van de gematchte user wordt uit de database gehaald
     // zodat je alleen de user die je hebt geliked/matched op de match pagina te zien krijgt
-    const match = await usersList.find({
+    const match = await usersList
+      .find({
         name: req.body.like,
       })
       .toArray();
@@ -160,13 +145,13 @@ app.post('/match', async (req, res, next) => {
     // als de gematchte waarde true is, dan heb je een match en wordt gerenderd naar match route
     if (updateLikedUsers(signedUser[0]) === true) {
       console.log(`you have a match with ${match[0].name} `);
-      res.render('match', {
-        users: match
+      res.render("match", {
+        users: match,
       });
       // als de gematchte waarde false is, wordt je teruggestuurd naar de index
     } else if (updateDislikedUsers(signedUser[0]) === false) {
       console.log(`no match.`);
-      res.redirect('/');
+      res.redirect("/");
     }
   } catch (error) {
     next(error);
@@ -174,23 +159,27 @@ app.post('/match', async (req, res, next) => {
 });
 
 // profile pagina van de gematchte baby wordt revealed naar de volwassen jochie.
-app.post('/profile', async (req, res) => {
+app.post("/profile", async (req, res) => {
   try {
-    const signedUser = await usersList.find({
-      name: req.session.name
-    }).toArray();
+    const signedUser = await usersList
+      .find({
+        name: req.session.name,
+      })
+      .toArray();
     const allLikedBabies = signedUser[0].liked;
     // zorgt voor dat de array niet telt vanaf 0
     // en die waarde wordt in likedUser meegegeven als de index
     const likedBaby = allLikedBabies.length - 1;
     const likedUser = allLikedBabies[likedBaby];
     // hele object van de gelikete user wordt uit de database gehaald
-    const showMatch = await usersList.find({
-      name: likedUser
-    }).toArray();
+    const showMatch = await usersList
+      .find({
+        name: likedUser,
+      })
+      .toArray();
     // rendert de gelikete user naar de profile pagina
-    res.render('profile', {
-      users: showMatch
+    res.render("profile", {
+      users: showMatch,
     });
   } catch (err) {
     res.status(404).send(err);
@@ -198,8 +187,8 @@ app.post('/profile', async (req, res) => {
 });
 
 // rendert error pagina
-app.get('/*', (req, res) => {
-  res.status(404).render('error');
+app.get("/*", (req, res) => {
+  res.status(404).render("error");
 });
 
 // Application running on port...
---
+++ src/models/users.js
@@ -0,0 +1,117 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+/*
+Om gebruik te maken van mongoose middlewares moet ik mijn eigen schema aanmaken
+en die vervolgens meegeven aan het user model.
+Op de userSchema kan ik dam verschillende methods op uitvoeren
+*/
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  age: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value <= 16) {
+        throw new Error("Je bent te jong voor deze applicatie");
+      }
+    },
+  },
+  img: {
+    type: String,
+  },
+  imgMatch: {
+    type: String,
+  },
+  imgOld: {
+      type: String,
+  },
+  location: {
+      type: String,
+  },
+  profession: {
+      type: String,
+  },
+  about: {
+      type: String,
+  },
+  password: {
+    type: String,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+// Creating custom method on our userSchema
+// https://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+
+/**
+ *
+ *https://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+ * Each Schema can define instance and static methods for its model.
+ * Statics are pretty much the same as methods but allow for defining functions that exist directly on your Model.
+ */
+
+//  Hier genereer ik dus een nieuwe token voor de gebruiker zodra hij inlogt. Deze token ga ik later gebruiken voor authenticatie
+userSchema.methods.generateAuthToken = async function () {
+  const user = this; // Makkelijker om naar te verwijzen
+  const token = jwt.sign(
+    {
+      _id: user._id.toString(),
+    } /* id is ObjectId(5ed0ef97405ebd524ada62d8).. jwt verwacht een string */,
+    process.env.JWT_SECRET
+  );
+
+  // Concat returned een nieuwe array samengevoegd met de nieuwe waardes
+  user.tokens = user.tokens.concat({ token }); // Zie user mode
+
+  await user.save(); // Sla de token op in mongo
+
+  return token;
+};
+
+/**
+ pre() is een functie die ik nu kan roepen op mijn userSchema
+ pre() is een method die wordt afgevuurd VOOR een event
+ Pre vraagt de arguments. Het eerste argument is het event.
+ in mijn geval is dat save. Dus voor het saven moet er iets gebeuren
+ Het tweede argument is de functie die moet worden uitgevoerd
+ De functie wordt in een andere contect geroepen.
+ Een arrow function gaat hier veel gezeik opleveren omdat die geen this binding hebben
+ */
+userSchema.pre("save", async function (next) {
+  /*
+  Hetgeen wat ik meegeef aan de model. Dat is dus de user met de data van req.body
+  zie server.js voor de kleine api route waar ik users aanmaak
+  */
+  const user = this;
+
+  /*
+Alleen het wachtwoord wijzigt willen we de hash functie toepassen.
+Dus niet elke keer als de user inlogt!
+*/
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8); // Zie playground brcypt.js
+  }
+
+  // Net als met middlewares in express, wordt next geroepen als de middleware is afgerond en model in dit geval kan worden opgeslagen
+  // Als next niet wordt geroepen, blijft de middleware hangen
+  next();
+});
+
+// Data validation & data sanitization
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

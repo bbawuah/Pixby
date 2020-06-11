d292278312e80bc2b4ab98ffb505fb9915bd62f1
bbawuah <brianbawuah@icloud.com>
2020-06-11 10:32:20 +0200

    removing req.session

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 // Connecten van database
 
 mongoose.connect(process.env.MONGODB_URL,  {
-    useNewUrlParser: false,
+    useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
